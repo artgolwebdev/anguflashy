fe8dcd8def018d4b73c39e297b2be6c192e84855
artgolwebdev <artyombb@gmail.com>
2023-10-01 13:41:56 +0300

    dev

--- src/app/welcome/welcome.component.ts
+++ src/app/welcome/welcome.component.ts
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./welcome.component.sass']
 })
 export class WelcomeComponent {
-
+    urls:any[] = [
+        "https://artgolwebdev.github.io/anguflashy/profile/1" , 
+        "https://artgolwebdev.github.io/anguflashy/profile/2"
+    ];
 }
